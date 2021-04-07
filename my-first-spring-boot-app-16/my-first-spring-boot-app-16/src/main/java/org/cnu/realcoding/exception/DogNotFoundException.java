6c54bd4dcf473a86d7799a358da6339e43a12358
YooJeongKyun <52439679+kyun1229@users.noreply.github.com>
2021-04-07 13:14:46 +0900

    Delete my-first-spring-boot-app-16/my-first-spring-boot-app-16 directory
    Delete overlap

--- my-first-spring-boot-app-16/my-first-spring-boot-app-16/src/main/java/org/cnu/realcoding/exception/DogNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.cnu.realcoding.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-public class DogNotFoundException extends RuntimeException{
-}
