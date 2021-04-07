8c9305096c2cd878ee9f9263d0eea727e1ec63a1
dayo2n <57654681+dayo2n@users.noreply.github.com>
2021-04-07 17:21:05 +0900

    removeDontUse() 구현

--- my-first-spring-boot-app-16/src/main/java/org/cnu/realcoding/controller/DogController.java
+++ my-first-spring-boot-app-16/src/main/java/org/cnu/realcoding/controller/DogController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class DogController {
 
@@ -38,5 +36,20 @@ public class DogController {
     @GetMapping("/dogs/findByOwnerPhoneNumber/{ownerPhoneNumber}")
     public Dog getDogByOwnerPhoneNumber(@PathVariable String ownerPhoneNumber){
         return dogManagementService.getDogByOwnerPhoneNumber(ownerPhoneNumber);
+    }
+
+    @PutMapping("/dogs/{prevName}/{newName}/{newKind}/{newOwnerName}/{newOwnerPhoneNumber}/{addMedicalRecords}")
+    public void updateDogInform(@PathVariable String prevName, @PathVariable String newName, @PathVariable String newKind, @PathVariable String newOwnerName, @PathVariable String newOwnerPhoneNumber, @PathVariable String addMedicalRecords){
+        dogManagementService.updateDog(prevName,newName,newKind,newOwnerName,newOwnerPhoneNumber,addMedicalRecords);
+    }
+
+    @PatchMapping("/dogs/updateKind/{name}/{kind}")
+    public void updateKind(@PathVariable String name, @PathVariable String kind) {
+        dogManagementService.updateKind(name, kind);
+    }
+
+    @PatchMapping("/dogs/updateMedicalRecords/{name}/{addMedicalRecords}")
+    public void updateMedicalRecords(@PathVariable String name, @PathVariable String addMedicalRecords) {
+        dogManagementService.updateMedicalRecords(name, addMedicalRecords);
     }
 }
--- my-first-spring-boot-app-16/src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ my-first-spring-boot-app-16/src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -1,6 +1,5 @@
 package org.cnu.realcoding.service;
 
-import lombok.Getter;
 import org.cnu.realcoding.domain.Dog;
 import org.cnu.realcoding.exception.DogNotFoundException;
 import org.cnu.realcoding.repository.DogRepository;
@@ -9,9 +8,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class DogManagementService {
@@ -41,12 +37,6 @@ public class DogManagementService {
         else{
             throw new DogNotFoundException();
         }
-//        if(dogRepository.findName(dog.getName())!=null || dogRepository.findOwnerName(dog.getOwnerName())!=null || dogRepository.findPhoneNum(dog.getOwnerPhoneNumber())!=null){
-//            dogRepository.insertDog(dog);
-//        }
-//        else{
-//            throw new DogNotFoundException();
-//        }
     }
 
     public Dog searchDog(String name, String ownerName, String ownerPhoneNumber){
@@ -84,5 +74,17 @@ public class DogManagementService {
             throw new DogNotFoundException();
         }
         return dog;
+    }
+
+    public void updateDog(String prevName, String newName, String newKind, String newOwnerName, String newOwnerPhoneNumber, String addMedicalRecords) {
+        dogRepository.updateDog(prevName, newName, newKind, newOwnerName, newOwnerPhoneNumber, addMedicalRecords);
+    }
+
+    public void updateKind(String name, String kind) {
+        dogRepository.updateKind(name, kind);
+    }
+
+    public void updateMedicalRecords(String name, String addMedicalRecords) {
+        dogRepository.updateMedicalRecords(name, addMedicalRecords);
     }
 }
