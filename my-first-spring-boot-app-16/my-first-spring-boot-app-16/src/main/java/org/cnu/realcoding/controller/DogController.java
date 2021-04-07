ce4fc12c7e00cd35ed6e4d53153479c9bdbfafab
YooJeongKyun <52439679+kyun1229@users.noreply.github.com>
2021-04-07 13:32:46 +0900

    add findDogByOwnerName()

---
+++ my-first-spring-boot-app-16/my-first-spring-boot-app-16/src/main/java/org/cnu/realcoding/controller/DogController.java
@@ -0,0 +1,34 @@
+package org.cnu.realcoding.controller;
+
+import org.cnu.realcoding.domain.Dog;
+import org.cnu.realcoding.service.DogManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DogController {
+
+    @Autowired
+    private DogManagementService dogManagementService;
+
+    @PostMapping("/dogs")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createDogs(@RequestBody Dog dog){
+        dogManagementService.insertDog(dog);
+    }
+
+    @GetMapping("/dogs")
+    public void getDog(@RequestBody Dog dog){
+        dogManagementService.searchDog(dog);
+    }
+
+    // localhost:8080/dogs/ian
+    @GetMapping("/dogs/{name}")
+    public Dog getDogByName(@PathVariable String name){
+        return dogManagementService.getDogByName(name);
+    }
+
+}
